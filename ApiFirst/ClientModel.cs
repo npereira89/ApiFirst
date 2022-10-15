b154727cd491fd4bc566d2825c2d7e21ea0ed8b5
Nuno Pereira <nuno.miguel.pereira@galp.com>
2022-10-15 19:38:19 +0100

    Add project files.

---
+++ ApiFirst/ClientModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ApiFirst
+{
+    public class ClientModel
+    {
+        public DateTime Date { get; set; }
+
+        public string Summary { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
---
+++ ApiFirst/Controllers/ClientController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFirst.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientCotroller : ControllerBase
+    {
+
+        private static readonly string[] Name_client = new[]
+        {
+            "Nuno", "Marta", "Filipa", "Catarina", "Helena", "Patricia", "Raquel"
+
+        };
+
+        private static readonly int[] Age = new[]
+        {
+            31,18,22,33,30,27,15
+
+        };
+
+        [HttpGet]
+        public IEnumerable<ClientModel> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 3).Select(index => new ClientModel
+            {
+
+                Date = DateTime.Now.AddDays(index),
+                Summary = Name_client[rng.Next(Name_client.Length)],
+                Age = Age[rng.Next(Age.Length)]
+
+            })
+            .ToArray();
+        }
+    }
+}
